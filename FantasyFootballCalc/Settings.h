acaed6e4562ca249de0fdc8bd3c428cdd8e12e1c
Justin Port <justin.m.port@gmail.com>
2014-07-07 19:45:21 -0400

    properties

--- FantasyFootballCalc/Settings.h
+++ FantasyFootballCalc/Settings.h
@@ -9,10 +9,28 @@
 #import <Foundation/Foundation.h>
 
 @interface Settings : NSObject
+    extern NSString *const PASSING_YARDS;
+    extern NSString *const PASSING_COMPLETION;
+    extern NSString *const PASSING_ATTEMPTS;
+    extern NSString *const PASSING_TD;
+    extern NSString *const PASSING_INT;
+    extern NSString *const RUSHING_YARDS;
+    extern NSString *const RUSHING_TD;
+    extern NSString *const RUSHING_ATTEMPS;
+    extern NSString *const RECEIVING_YARDS;
+    extern NSString *const RECEIVING_RECEPTIONS;
+    extern NSString *const RECEIVING_TD;
+    extern NSString *const KICKING_XP;
+    extern NSString *const KICKING_FG;
+    extern NSString *const KICKING_FG50;
+    extern NSString *const DEFENSE_TD;
+    extern NSString *const DEFENSE_INTERCEPTION;
+    extern NSString *const DEFENSE_SACK;
+    extern NSString *const DEFENSE_SAFETY;
 
  - (void) setProperty:(NSString *)property:(NSString *)value;
  - (NSString *)getProperty:(NSString *)property;
  - (bool)propertyExists:(NSString *)property;
-
+ - (void)resetTable;
 
 @end
