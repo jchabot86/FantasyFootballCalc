4ddc333f8cba3ccf4a86c80659c475abdde875ea
Justin Port <justin.m.port@gmail.com>
2014-07-09 21:17:49 -0400

    yupppp

--- FantasyFootballCalc/SettingsViewController.m
+++ FantasyFootballCalc/SettingsViewController.m
@@ -50,14 +50,32 @@
 
 
 -(void)cancelNumberPad{
-    [PassingTd resignFirstResponder];
-    PassingTd.text = @"";
+    //[PassingTd resignFirstResponder];
+    //PassingTd.text = @"";
 }
 
 -(void)doneWithNumberPad{
-    NSString *numberFromTheKeyboard = PassingTd.text;
+    //NSString *numberFromTheKeyboard = PassingTd.text;
     [PassingTd resignFirstResponder];
-    PassingTd.text = numberFromTheKeyboard;
+    [PassingTd resignFirstResponder];
+    [PassingYards resignFirstResponder];
+    [PassingCompletion resignFirstResponder];
+    [PassingAttempts resignFirstResponder];
+    [PassingInt resignFirstResponder];
+    [RushingYards resignFirstResponder];
+    [RushingTd resignFirstResponder];
+    [RushingAttempts resignFirstResponder];
+    [ReceivingYards resignFirstResponder];
+    [ReceivingReceptions resignFirstResponder];
+    [ReceivingTd resignFirstResponder];
+    [KickingXp resignFirstResponder];
+    [KickingFg resignFirstResponder];
+    [KickingFg50 resignFirstResponder];
+    [DefenseTd resignFirstResponder];
+    [DefenseInterception resignFirstResponder];
+    [DefenseSack resignFirstResponder];
+    [DefenseSafety resignFirstResponder];
+    //PassingTd.inputAccessoryView = numberFromTheKeyboard;
 }
 
 
@@ -67,14 +85,38 @@
     // Do any additional setup after loading the view.
     UIToolbar* numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
     numberToolbar.barStyle = UIBarStyleBlackTranslucent;
-    numberToolbar.items = [NSArray arrayWithObjects:
+    /*numberToolbar.items = [NSArray arrayWithObjects:
                            [[UIBarButtonItem alloc]initWithTitle:@"Cancel" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelNumberPad)],
                            [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
                            [[UIBarButtonItem alloc]initWithTitle:@"Apply" style:UIBarButtonItemStyleDone target:self action:@selector(doneWithNumberPad)],
+                           nil];*/
+    
+    numberToolbar.items = [NSArray arrayWithObjects:
+                           [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                           [[UIBarButtonItem alloc]initWithTitle:@"Done" style:UIBarButtonItemStyleDone target:self action:@selector(doneWithNumberPad)],
                            nil];
+
     
     [numberToolbar sizeToFit];
+
     PassingTd.inputAccessoryView = numberToolbar;
+    PassingYards.inputAccessoryView = numberToolbar;
+    PassingCompletion.inputAccessoryView = numberToolbar;
+    PassingAttempts.inputAccessoryView = numberToolbar;
+    PassingInt.inputAccessoryView = numberToolbar;
+    RushingYards.inputAccessoryView = numberToolbar;
+    RushingTd.inputAccessoryView = numberToolbar;
+    RushingAttempts.inputAccessoryView = numberToolbar;
+    ReceivingYards.inputAccessoryView = numberToolbar;
+    ReceivingReceptions.inputAccessoryView = numberToolbar;
+    ReceivingTd.inputAccessoryView = numberToolbar;
+    KickingXp.inputAccessoryView = numberToolbar;
+    KickingFg.inputAccessoryView = numberToolbar;
+    KickingFg50.inputAccessoryView = numberToolbar;
+    DefenseTd.inputAccessoryView = numberToolbar;
+    DefenseInterception.inputAccessoryView = numberToolbar;
+    DefenseSack.inputAccessoryView = numberToolbar;
+    DefenseSafety.inputAccessoryView = numberToolbar;
     [self loadSettings];
 }
 
@@ -95,12 +137,46 @@
     Settings* properties = [Settings new];
     
     [properties setProperty:PASSING_TD:PassingTd.text];
-    NSLog(@"Test: %@",PassingTd.text);
+    [properties setProperty:PASSING_YARDS:PassingYards.text];
+    [properties setProperty:PASSING_COMPLETION:PassingCompletion.text];
+    [properties setProperty:PASSING_ATTEMPTS:PassingAttempts.text];
+    [properties setProperty:PASSING_INT:PassingInt.text];
+    [properties setProperty:RUSHING_YARDS:RushingYards.text];
+    [properties setProperty:RUSHING_TD:RushingTd.text];
+    [properties setProperty:RUSHING_ATTEMPS:RushingAttempts.text];
+    [properties setProperty:RECEIVING_YARDS:ReceivingYards.text];
+    [properties setProperty:RECEIVING_RECEPTIONS:ReceivingReceptions.text];
+    [properties setProperty:RECEIVING_TD:ReceivingTd.text];
+    [properties setProperty:KICKING_XP:KickingXp.text];
+    [properties setProperty:KICKING_FG:KickingFg.text];
+    [properties setProperty:KICKING_FG50:KickingFg50.text];
+    [properties setProperty:PASSING_TD:DefenseTd.text];
+    [properties setProperty:DEFENSE_INTERCEPTION:DefenseInterception.text];
+    [properties setProperty:DEFENSE_SACK:DefenseSack.text];
+    [properties setProperty:DEFENSE_SAFETY:DefenseSafety.text];
+    //NSLog(@"Test: %@",PassingTd.text);
 }
 
 - (void) loadSettings{
     Settings* properties = [Settings new];
     PassingTd.text = [properties getProperty:PASSING_TD];
+    PassingYards.text = [properties getProperty:PASSING_YARDS];
+    PassingCompletion.text = [properties getProperty:PASSING_COMPLETION];
+    PassingAttempts.text = [properties getProperty:PASSING_ATTEMPTS];
+    PassingInt.text = [properties getProperty:PASSING_INT];
+    RushingYards.text = [properties getProperty:RUSHING_YARDS];
+    RushingTd.text = [properties getProperty:RUSHING_TD];
+    RushingAttempts.text = [properties getProperty:RUSHING_ATTEMPS];
+    ReceivingYards.text = [properties getProperty:RECEIVING_YARDS];
+    ReceivingReceptions.text = [properties getProperty:RECEIVING_RECEPTIONS];
+    ReceivingTd.text = [properties getProperty:RECEIVING_TD];
+    KickingXp.text = [properties getProperty:KICKING_XP];
+    KickingFg.text = [properties getProperty:KICKING_FG];
+    KickingFg50.text = [properties getProperty:KICKING_FG50];
+    DefenseTd.text = [properties getProperty:PASSING_TD];
+    DefenseInterception.text = [properties getProperty:DEFENSE_INTERCEPTION];
+    DefenseSack.text = [properties getProperty:DEFENSE_SACK];
+    DefenseSafety.text = [properties getProperty:DEFENSE_SAFETY];
 }
 
 
