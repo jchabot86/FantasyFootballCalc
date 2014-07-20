b983dbc5e618a56ce664c46ad647a22119b4c36d
Justin Port <justin.m.port@gmail.com>
2014-07-20 01:07:20 -0400

    calculation refresh

--- FantasyFootballCalc/SettingsViewController.m
+++ FantasyFootballCalc/SettingsViewController.m
@@ -207,6 +207,8 @@
     [properties setProperty:DEFENSE_INTERCEPTION:DefenseInterception.text];
     [properties setProperty:DEFENSE_SACK:DefenseSack.text];
     [properties setProperty:DEFENSE_SAFETY:DefenseSafety.text];
+    
+    [properties refreshScores];
 /*
         float calcPassingYards = 0;
         float calcPassingTd = 0;
